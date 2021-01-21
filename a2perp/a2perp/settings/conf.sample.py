eba8ec98b01b31a8cda963dcd15612a9393e8a46
Leo Portemont <leo.portemont@portemont.eu>
2021-01-21 19:58:38 +0100

    Add example CLIENT_ID and CLIENT_SECRET to prologin settings

--- a2perp/a2perp/settings/conf.sample.py
+++ a2perp/a2perp/settings/conf.sample.py
@@ -2,3 +2,9 @@ from .common import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['a2perp.prologin.dev']
+
+# To obtain a Client ID and Client Secret you must contact
+# a Prologin Root, the redirect uri will be https://<site_host>:<site_port>/social/login/prologin
+
+SOCIAL_AUTH_PROLOGIN_KEY = 'CHANGE_ME' # This is the client ID given by a Prologin Root
+SOCIAL_AUTH_PROLOGIN_SECRET = 'CHANGE_ME' # This is the client secret given by a Prologin Root
