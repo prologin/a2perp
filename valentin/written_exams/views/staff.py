f76580edad857a2a88f36f1f66f2c1c6b0c39484
Leo Portemont <leo.portemont@portemont.eu>
2021-01-26 09:17:01 +0100

    written-exams: add json feedback on form testing views

--- valentin/written_exams/views/staff.py
+++ valentin/written_exams/views/staff.py
@@ -8,6 +8,17 @@ from django.views.generic import ListView
 class FormTestingView(EnsureStaffMixin, se_views.BaseSEFormView):
     template_name = 'written_exams/form-testing.html'
 
+    def form_valid(self, form):
+        return JsonResponse({'is_valid': True, 'cleaned_data': form.cleaned_data })
+
+    def form_invalid(self, form):
+        dic = {
+            'is_valid': False,
+            'data': form.data,
+            'errors': form.errors,
+        }
+        return JsonResponse(dic)
+
     def get_success_url(self):
         return reverse('written_exams:form-testing', args=[self.form_instance.id])
 
