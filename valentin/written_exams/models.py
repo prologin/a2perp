7a79ac806a9b5d326fa30ea7eceb998b0af193d7
Leo Portemont <leo.portemont@portemont.eu>
2021-02-13 23:07:30 +0100

    written_exams: override last updated file

--- valentin/written_exams/models.py
+++ valentin/written_exams/models.py
@@ -1,13 +1,22 @@
+from django.core.files.storage import FileSystemStorage
+import os
 from django.db import models
 from django.contrib.auth import get_user_model
 from pathlib import Path
+
+
+class OverrideStorage(FileSystemStorage):
+    OS_OPEN_FLAGS = os.O_CREAT | os.O_WRONLY | getattr(os, "O_BINARY", 0)
+
+    def get_available_name(self, name, max_length=None):
+        return name
 
 
 def get_path(sub, filename):
     id = f"upstream_id_not_found_{sub.user.id}"
     if not (social := sub.user.social_auth.first()) is None:
         id = social.uid
-    return Path("exams_submissions") / str(sub.session.id) / str(id) / filename
+    return Path("exams_submissions") / str(sub.session.id) / f"{str(id)}.pdf"
 
 
 class Submission(models.Model):
@@ -21,7 +30,7 @@ class Submission(models.Model):
         to="semifinals.Session", on_delete=models.CASCADE
     )
     last_updated = models.DateTimeField(null=True, blank=True)
-    file = models.FileField(upload_to=get_path)
+    file = models.FileField(upload_to=get_path, storage=OverrideStorage())
 
     def __str__(self):
         return f"{self.user}@{self.session}"
